99b14e04a8770902567051de9a444c6d79b68ac8
J <jared.perez.ny@gmail.com>
2025-05-04 00:27:02 -0400

    Create NYS_Analysis.py
    Analysis File where data analysis and validation can happen. Although some sample code and validation code are in the script, additional code can be found in README.md

---
+++ NYS_Analysis.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 23:11:50 2025
+
+@author: Jared
+"""
+
+#A simple map project, showing decline of employment in the manufacturing sector as well as county 
+#level poverty rates
+
+#Libraries
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import NYS_Functions as apa
+#Importing Files____________________________________________________________________________
+data = pd.read_csv('GoodData.csv')
+GeoData = gpd.read_file("my_shapes.gpkg")
+#____________Data Maniuplation and Validation______________
+data_2 = data[(data["Month"] == 0) & (data["Title"] == "Manufacturing")]
+service_produce = data[data["Title"] == "Service-Providing"].copy()
+comparison = data[data["Title"].isin(["Service-Providing", "Goods Producing"])].copy()
+#Data Validation, to ensure that "New York State" was removed.
+csv_names = set(pd.read_csv('GoodData.csv')['Area Name'].unique())
+shape_names = set(GeoData['Area Name'].unique())
+missing_in_shapes = csv_names - shape_names
+print(f"Area Names in CSV but missing in my_shapes.gpkg:, {missing_in_shapes}")
+#__________________________________________________________________________________________
+#Data Analysis Sample
+df_result = apa.sector_pct_change(1998, 2006, data, ["Manufacturing", "Construction"], False, "Rochester Metro Area", True)
+
